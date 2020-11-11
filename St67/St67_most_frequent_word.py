d6d3a1146e64725f4eb67fcd6a81bf4703a5d56e
Ilya Zooloos Popov <zooloosik@gmail.com>
2020-11-11 12:13:55 +0300

    done with most_frequent_word(), add dataset file for it

--- St67/St67_most_frequent_word.py
+++ St67/St67_most_frequent_word.py
@@ -1,12 +1,23 @@
+'''
+Приложение читает текстовый файл и выводит наиболее часто встречающееся слово и количество его повторений.
+Примечание:
+  * Наличие пунктуации в исходнике допустимо, но подсчёт будет вестись не корректно, словом считается текст между пробельными символами.
+  * Регистр слов/букв не учитывается специально.
+  * Если слов с максимальным повторением несколько, то выводится "наименьшее" в лексикографическом смысле.
+'''
 def most_frequent_word():
     import os
 
     path = os.path.join('.', 'St67_most_frequent_word_input.txt')
     with open(path, 'r') as inf:
-        s = [inf.readline().strip().split() for _ in inf]
+        s_list = []
+        [s_list.extend(el) for el in [line.lower().strip().split() for line in inf]]
 
-    print(s)
+    s_dict = {}
+    [s_dict.update({el: s_list.count(el)}) for el in set(s_list)]
 
+    #max(s_dict.values())
+    print(min([el for el in s_dict.keys() if s_dict[el] == max(s_dict.values())]), max(s_dict.values()))
     
     #with open('St67_most_frequent_word_output.txt', 'w') as ouf:
     #    ouf.write(s_out)

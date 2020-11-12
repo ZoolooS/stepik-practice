8873de5433e57a71fb4fb55203115ffc4d0faf72
Ilya Zooloos Popov <zooloosik@gmail.com>
2020-11-12 11:54:52 +0300

    refactoring recurrent_file_reading.py

--- St67/St67_recurrent_file_reading.py
+++ St67/St67_recurrent_file_reading.py
@@ -9,43 +9,44 @@ https://stepic.org/media/attachments/course67/3.6.3/
 # ====== imports block ================================== #
 import requests, os, io
 
-# ====== defining functions ============================= #
-#def bubo_read_and_return_file(rel_path='.', strip='no', split=('no')):  # Вставить из библиотеки
-
-
-def return_first_url():
+# ====== function declaration =========================== #
+def get_first_url():
     path = os.path.join('.', 'St67_recurrent_file_reading_input.txt')
-    #with io.open(path, 'r', encoding='utf-8') as inf:
-    with open(path, 'r') as inf:
+    with io.open(path, 'r', encoding='utf-8') as inf:
         return inf.readline().strip()  # first url
 
 
-def recurrence_by_url(url):
-    base = 'https://stepic.org/media/attachments/course67/3.6.3/'
-    r = requests.get(url).text.strip()
-
-    if r.split()[0] != 'We':
-        with open('St67_recurrent_file_reading_link_list.txt', 'a') as ouf:
-            ouf.write(base + r + '\n')
-        recurrence_by_url(base + r)
-    
-    if r.split()[0] == 'We':
-        print('We? ==>', r.split()[0])
-        print('r.text ============\n' + r + '\n===================')
-        print('r.text =', type(r))
-
-    return r  # s_out
+def get_content_from_url(url):
+    return requests.get(url).text.strip()  # s_out
 
 
 def print_to_file(s_out):
-    print('s_out =', type(s_out))
+    #print('s_out =', type(s_out))
     print('s_out ============\n' + s_out + '\n===================')
-
     with open('St67_recurrent_file_reading_output.txt', 'w') as ouf:
         ouf.write(s_out)
 
 
+def main():
+    base = 'https://stepic.org/media/attachments/course67/3.6.3/'
+    url = get_first_url()
+    s_out = get_content_from_url(url)
+
+    while s_out.split()[0] != 'We':
+        with open('St67_recurrent_file_reading_link_list.txt', 'a') as ouf:
+            ouf.write(base + s_out + '\n')
+        s_out = get_content_from_url(base + s_out)
+
+    if s_out.split()[0] == 'We':
+        #print('We? ==>', s_out.split()[0])
+        #print('s_out ============\n' + s_out + '\n===================')
+        #print('s_out is', type(s_out))
+        print_to_file(s_out)
+    else:
+        print('Что-то пошло не так..')
+
+
 # ====== main code ====================================== #
-print_to_file(recurrence_by_url(return_first_url()))
+main()
 
 # ====== end of code ==================================== #
